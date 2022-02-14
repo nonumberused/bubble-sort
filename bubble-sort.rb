dc17a8996bb10c003f9a9291a1fcabe324d9daf4
Adam <nonumberused@gmail.com>
2022-02-14 11:22:41 +0700

    Added second version without #sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,5 +1,17 @@
 def bubble_sort(array)
-  p array.sort { |a, b| a <=> b}
+  n = array.length - 1
+
+  loop do
+    sorted = true
+    for i in 0..(n - 1)
+      if array[i] > array[i + 1]
+        array[i + 1], array[i] = array[i], array[i + 1]
+        sorted = false
+      end
+    end
+    break if sorted == true
+  end
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
